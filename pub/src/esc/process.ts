0905170386a3018d45f894d850d89c297a4ce51b
corno <gitkraken.a@schraverus.com>
2022-02-05 14:22:29 +0100

    removed onDone

--- pub/src/esc/process.ts
+++ pub/src/esc/process.ts
@@ -5,20 +5,20 @@ export function runProgram(
         $: {
             argument?: string
         },
-        $i: {
-            onDone: ($: {
-                success: boolean,
-            }) => void
-        }
+        // $i: {
+        //     onDone: ($: {
+        //         success: boolean,
+        //     }) => void
+        // }
     ) => void
 ): void {
     const pa = process.argv
-    let isDone = false
-    process.on("beforeExit", () => {
-        if (!isDone) {
-            throw new Error("process did not terminate properly, isDone() is never called")
-        }
-    })
+    // let isDone = false
+    // process.on("beforeExit", () => {
+    //     if (!isDone) {
+    //         throw new Error("process did not terminate properly, isDone() is never called")
+    //     }
+    // })
     if (pa.length > 3) { //expected format is 'node <scriptname> <argument>'
         console.error("expecting either 0 or 1 argument, not more")
         processExit(1)
@@ -27,16 +27,16 @@ export function runProgram(
             {
                 argument: pa[2],
             },
-            {
-                onDone: ($) => {
-                    if (isDone) {
-                        throw new Error("process did not terminate properly, isDone() is called multiple times")
-                    }
-                    if (!$.success) {
-                        process.exit(1)
-                    }
-                }
-            }
+            // {
+            //     onDone: ($) => {
+            //         if (isDone) {
+            //             throw new Error("process did not terminate properly, isDone() is called multiple times")
+            //         }
+            //         if (!$.success) {
+            //             process.exit(1)
+            //         }
+            //     }
+            // }
         )
     }
 }
